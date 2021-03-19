0712733a6d1e89a6ec509b8314422b36a3ec7230
TristanHubel <t.hubel@codeelements.de>
2021-03-19 13:57:14 +0100

    add purge src dirs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-    purge: [],
+    purge: [
+    './src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.jsx',],
     theme: {
       fontFamily: {
         'sans': 'Roboto, Arial, sans-serif',
