dd547094b92879174df5202f07546c358c201072
tristanhubel <t.hubel@gmail.com>
2021-03-21 18:53:33 +0100

    fix typo and hover state

--- pages/[language]/blog/index.js
+++ pages/[language]/blog/index.js
@@ -43,11 +43,12 @@ export default class extends React.Component {
             {posts.map((post) => {
               const lang = post.lang === "default" ? "/en" : `/${post.lang}`;
               return (
-                <li
-                  key={post.slug}
-                  className="px-10 my-4 py-6 rounded-lg bg-white shadow-xl rounded-xl p-5 dark:bg-gray-900"
-                >
-                <div class="-my-8 divide-y-2 divide-gray-100">
+                <li key={post.slug}>
+                  <a
+                    href={`${lang}/blog/${post.slug}`}
+                    className="px-10 my-4 py-6 rounded-lg bg-white shadow-xl rounded-xl p-5 dark:bg-gray-900 block transition hover:opacity-50"
+                  >
+                <div class="-my-8 divide-y-2 divide-gray-100" >
                     <div class="py-8 flex flex-wrap md:flex-nowrap">
                       <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
                         <span class="font-semibold title-font text-gray-700">CATEGORY</span>
@@ -60,14 +61,12 @@ export default class extends React.Component {
                       </div>
                       <div class="md:flex-grow">
                         <a
-                          className="text-2xl text-gray-700 font-bold hover:text-gray-600"
-                          href={`${lang}/blog/${post.slug}`}
+                          className="text-2xl text-gray-700 font-bold"
                         >
                           <h2 class="text-2xl font-medium text-gray-900 dark:text-gray-50 title-font mb-2">{post.content.title}</h2>
                         </a>
                         <p class="leading-relaxed text-gray-900 dark:text-gray-50">{post.content.intro}</p>
                         <a class="text-indigo-500 inline-flex items-center mt-4"
-                            href={`${lang}/blog/${post.slug}`}
                           >Read more
                           <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
                             <path d="M5 12h14"></path>
@@ -77,6 +76,7 @@ export default class extends React.Component {
                       </div>
                     </div>
                   </div>
+                </a>
                 </li>
               );
             })}
